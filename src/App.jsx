b559fdb29d43675636de4dd7f7bb48a811c11818
jsguerra <jose@gcostudios.com>
2023-01-06 11:17:52 +0000

    basic router configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { About } from "./components/About";
+import { Home } from "./components/Home";
 
 function App() {
-  return <h1>hello</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Routes>
+  );
 }
 
 export default App;
