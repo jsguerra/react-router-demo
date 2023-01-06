ee592c0df77340e3e340fca12906b575c2cefba1
jsguerra <jose@gcostudios.com>
2023-01-06 17:59:19 +0000

    added nested routes

---
+++ src/components/Products.jsx
@@ -0,0 +1,17 @@
+import { Link, Outlet } from "react-router-dom";
+
+export const Products = () => {
+  return (
+    <>
+      <div>
+        <input type="search" placeholder="Search Products" />
+      </div>
+      <nav>
+        {/* do not include forward slash in route */}
+        <Link to="featured">Featured</Link>
+        <Link to="new">New</Link>
+      </nav>
+      <Outlet />
+    </>
+  );
+};
