92486e1615aa7052e85fe72b709bbd9c0483143e
jsguerra <jose@gcostudios.com>
2023-01-07 11:29:45 +0000

    dynamic routes

---
+++ src/components/Users.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+
+export const Users = () => {
+  return (
+    <div>
+      <h2><Link to="/users/1">User 1</Link></h2>
+      <h2><Link to="/users/2">User 2</Link></h2>
+      <h2><Link to="/users/3">User 3</Link></h2>
+      <h2><Link to="/users/admin">Admin User</Link></h2>
+      <Outlet />
+    </div>
+  );
+};
